1b3d9761dac4ae78db061ee6d4bada160bd0f77a
Kiflan Adli <kiflanadli@gmail.com>
2023-01-14 13:31:21 +0700

    load data & layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { useEffect, useMemo, useState } from "react";
 import Header from "./components/Header";
+import Outputs from "./components/Outputs";
 import Maps from "./components/Maps";
+import useAxiosGet from "./hooks/HttpRequests";
+import "./styles/ip-section.scss";
 
 function App() {
+  const [input, setInput] = useState("");
+  const handleSearch = (value) => setInput(value);
+  const url = `https://geo.ipify.org/api/v2/country,city?apiKey=at_sky8VeFQENaUN22Jf8beDQPhvka9Y&ipAddress=${input}&domain=${input}`;
+  const output = useAxiosGet(url);
+  useEffect(() => {
+    if (input === "" && output.data)
+      localStorage.setItem("data", JSON.stringify(output.data));
+  }, [input, output.data]);
+
   return (
-    <div>
-      <Header />
-      <Maps />
-    </div>
+    <>
+      <section
+        className="ip-section"
+        style={{ backgroundImage: "url('/images/pattern-bg.png')" }}
+      >
+        <Header handleSearch={handleSearch} />
+        <article className="output">
+          <Outputs output={output} />
+        </article>
+      </section>
+      <section>
+        <Maps data={output.data} />
+      </section>
+    </>
   );
 }
 
